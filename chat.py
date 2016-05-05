3c526a1776140df36b85640b0d8b083e4c8650ba
Steve Stagg <stestagg@gmail.com>
2016-05-05 21:00:58 +0100

    Main loop

--- chat.py
+++ chat.py
@@ -1,24 +1,38 @@
-
+import select
+import sys
 import networkzero as nw0
 
 
 def main():
-    
+
     username = raw_input("Please enter your username: ")
-    
+
     address = nw0.advertise("zerochat_" + username)
-    
+
     oldchats = {}
     while True:
-        
+
         chats = {name:address for name, address in nw0.discover_all() if name.startswith("zerochat_")}
         if chats.keys() != oldchats.keys():
             print "New users:"
             print chats
-        
-        
+
+        rlist, wlist, elist = select.select([sys.stdin], [], [])
+        if rlist:
+            recipient = sys.stdin.readline()
+            msg = raw_input("Message:")
+            print "\x1b[31;m[%s] %s\x1b[0m" % (recipient, msg)
+
+            recipient_service = "zerochat_" + recipient
+            nw0.send_message_to(recipient_service, msg)
+
+
+        received_msg = nw0.wait_for_message_from(address, wait_for_s=0, autoreply=True)
+        if received_msg is not None:
+            print "Got message: %s" % received_msg
+
         oldchats = chats
-        
+
 
 
 if __name__ == '__main__':
