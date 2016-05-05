a3f38c8a5b6cabcd1e071224de0e2776017df6bd
Matt Wheeler <m@funkyhat.org>
2016-05-05 21:09:13 +0100

    py3 fixes

--- chat.py
+++ chat.py
@@ -3,9 +3,15 @@ import sys
 import networkzero as nw0
 
 
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
 def main():
 
-    username = raw_input("Please enter your username: ")
+    username = input("Please enter your username: ")
 
     address = nw0.advertise("zerochat_" + username)
 
@@ -14,22 +20,22 @@ def main():
 
         chats = {name:address for name, address in nw0.discover_all() if name.startswith("zerochat_")}
         if chats.keys() != oldchats.keys():
-            print "New users:"
-            print chats
+            print("New users:")
+            print(chats)
 
-        rlist, wlist, elist = select.select([sys.stdin], [], [])
+        rlist, wlist, elist = select.select([sys.stdin], [], [], 0.1)
         if rlist:
             recipient = sys.stdin.readline()
-            msg = raw_input("Message:")
-            print "\x1b[31;m[%s] %s\x1b[0m" % (recipient, msg)
+            msg = input("Message:")
+            print("\x1b[31;m[%s] %s\x1b[0m" % (recipient, msg))
 
-            recipient_service = "zerochat_" + recipient
+            recipient_service = nw0.discover("zerochat_" + recipient)
             nw0.send_message_to(recipient_service, msg)
 
 
         received_msg = nw0.wait_for_message_from(address, wait_for_s=0, autoreply=True)
         if received_msg is not None:
-            print "Got message: %s" % received_msg
+            print("Got message: %s" % received_msg)
 
         oldchats = chats
 
